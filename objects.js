e0450c6eb232fa496c8bee9502bd1475c6159ef3
cwoolnoughchamorro@googlemail.com <cwoolnoughchamorro@googlemail.com>
2018-08-20 13:47:47 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return playlist['My Bloody Valentine'] = Sometimes;
+  return playlist['artistName'] = songTitle;
 }
 
 function removeFromPlaylist(playlist, artistName){

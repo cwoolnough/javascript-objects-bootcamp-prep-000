9873f9ca87ce2d3deff3cb9b26f8e701c0238e64
cwoolnoughchamorro@googlemail.com <cwoolnoughchamorro@googlemail.com>
2018-08-20 13:26:50 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {
+  Slowdive: 'Alison'
+}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  return object.
+}
+
+function removeFromPlaylist(playlist, artistName){
+  return delete playlist.artistName;
+}
